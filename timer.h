85de81987903600eae2dd045a0296e30f40505b0
dark6 <dark69archer@gmail.com>
2022-10-30 23:15:20 +0700

    1

---
+++ timer.h
@@ -0,0 +1,15 @@
+//
+// Created by dark6 on 30.10.2022.
+//
+
+#ifndef LABA_1_TMP_TIMER_H
+#define LABA_1_TMP_TIMER_H
+
+
+class timer {
+void start();
+int stop();
+};
+
+
+#endif //LABA_1_TMP_TIMER_H
